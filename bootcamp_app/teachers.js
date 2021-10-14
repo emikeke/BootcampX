cd39b5c23f4cfbbe956229979c1e574d4881bf60
Emily K <emily.ke@hotmail.com>
2021-10-14 02:07:11 -0400

    SQL injection, parameterized query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,23 +1,35 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
+const pool = new Pool ({
   user: 'emi',
   password: '123',
   host: 'localhost',
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = ['%' + cohortName + '%'];
+
+pool.connect((err) => {
+  if (err) throw err;
+  console.log('connected to database');
+
 pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+SELECT distinct teachers.name AS teacher, cohorts.name AS cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`, values)
 .then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
+  res.rows.forEach(user => {
+    console.log(`${user.cohort} has a teacher ${user.teacher}`);
+  });
+})
+.catch(err => console.error('query error', err.stack));
+
 });
